d310336383f28629bd7f5fba83c06758bfce499b
Bast <bastien.last87@gmail.com>
2020-06-23 14:08:42 +0200

    Basic routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,7 +20,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-$routes->setAutoRoute(true);
+$routes->setAutoRoute(false);
 
 /**
  * --------------------------------------------------------------------
@@ -31,6 +31,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('(:any)', 'Pages::view/$1');
 
 /**
  * --------------------------------------------------------------------
