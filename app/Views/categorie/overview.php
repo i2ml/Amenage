893222bc171cae866418a004e185bb444a645e3e
Bast <bastien.last87@gmail.com>
2020-07-13 11:28:57 +0200

    Categories page layout

--- app/Views/categorie/overview.php
+++ app/Views/categorie/overview.php
@@ -1,30 +1,36 @@
-<h2><?= esc($title); ?></h2>
+<div class="row justify-content-center m-5">
+    <h2><?= esc($title); ?></h2>
+</div>
+<div class="container"></div>
+<div class="row justify-content-md-center">
+    <?php if (!empty($categorie) && is_array($categorie)) :
 
-<?php if (!empty($categorie) && is_array($categorie)) : ?>
-
-    <?php foreach ($categorie as $categorie_item) : ?>
-
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="h5 font-weight-bold text-warning text-uppercase mb-1"><?= esc($categorie_item['nom']); ?></div>
-                            <div class="small mb-0 font-weight-bold text-gray-800">Nombre d'aides techniques : <?= esc($categorie_item['aideTechAmount']); ?></div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+        $colors = array("primary", "secondary", "info", "warning", "danger", "success", "primary", "secondary", "info", "warning", "danger", "success", "primary", "secondary", "info", "warning", "danger", "success");
+        $emoji = array("fas fa-dice", "fas fa-hands", "fas fa-people-arrows", "fas fa-shower", "fas fa-lock", "fas fa-route", "fas fa-couch", "fas fa-couch", "fas fa-bullhorn", "fas fa-dice", "fas fa-glasses", "fas fa-headset");
+        foreach ($categorie as $categorie_item) : ?>
+            <!-- Pending Requests Card Example -->
+            
+                <div class="col-md-5 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="h5 font-weight-bold text-primary text-uppercase mb-1"><?= esc($categorie_item['nom']); ?></div>
+                                    <div class="small mb-0 font-weight-bold text-gray-800">Nombre d'aides techniques : <?= esc($categorie_item['aideTechAmount']); ?></div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="<?= esc($emoji[$categorie_item['id']]); ?> fa-2x text-gray-300"></i>
+                                </div>
+                                <a href="aideTechnique" class="stretched-link"></a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        </div>
-    <?php endforeach; ?>
-
+      
+        <?php endforeach; ?>
+</div>
 <?php else :
-    // TODO : faire la partie ci-dessous
+        // TODO : faire la partie ci-dessous
 ?>
 
     <h3>No News</h3>
