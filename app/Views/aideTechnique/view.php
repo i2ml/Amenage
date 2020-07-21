f0b4fe404caa1e0a9de3883d7002300ef2258dd7
Bast <bastien.last87@gmail.com>
2020-07-21 13:30:01 +0200

    Page aideTechnique

--- app/Views/aideTechnique/view.php
+++ app/Views/aideTechnique/view.php
@@ -34,7 +34,7 @@
                 <div class="row">
                     <div class="col-md-auto">
                         <h6 class="d-inline m-0 font-weight-bold text-primary">Catégorie : </h6>
-                        <h6 class="d-inline"><?= ucfirst(esc($aideTechnique['nomCat'])); ?></h6>
+                        <h6 class="d-inline"><?= ucfirst(esc($aideTechnique['nomCat'])); ?>.</h6>
                     </div>
                 </div>
                 <div class="row">
@@ -43,7 +43,7 @@
                         <h6 class="d-inline">
                             <?php if (esc($aideTechnique['idDim']) === null) {
                             ?>
-                                Information inconnue ou non applicable
+                                Information inconnue ou non applicable.
                                 <?php
                             }
                             if (esc($aideTechnique['longueurMax']) != 0 && esc($aideTechnique['longueurMax']) != null) {
@@ -91,7 +91,7 @@
                         <?php
                         if (esc($aideTechnique['idPrix']) === null) {
                         ?>
-                            <h6 class="d-inline"> Information inconnue ou non applicable</h6>
+                            <h6 class="d-inline"> Information inconnue ou non applicable.</h6>
                         <?php
                         } else if (esc($aideTechnique['prixMin']) >= esc($aideTechnique['prixMax'])) {
                         ?>
@@ -113,7 +113,7 @@
                         <?php
                         if (esc($aideTechnique['idPec']) === null) {
                         ?>
-                            <h6 class="d-inline"> Information inconnue ou non applicable</h6>
+                            <h6 class="d-inline"> Information inconnue ou non applicable.</h6>
                         <?php
                         } else if (esc($aideTechnique['priseEnChargeMin']) >= esc($aideTechnique['priseEnChargeMax'])) {
                         ?>
@@ -134,16 +134,16 @@
                         <?php
                         if (esc($aideTechnique['idPoids']) === null) {
                         ?>
-                            <h6 class="d-inline"> Information inconnue ou non applicable</h6>
+                            <h6 class="d-inline"> Information inconnue ou non applicable.</h6>
                         <?php
                         } else if (esc($aideTechnique['poidsMin']) >= esc($aideTechnique['poidsMax'])) {
                         ?>
                             <h6 class="d-inline"> environ
                                 <?php
                                 if ($aideTechnique['poidsMax'] < 999) {
-                                    echo (esc($aideTechnique['poidsMax']) . " g");
+                                    echo (esc($aideTechnique['poidsMax']) . "g");
                                 } else {
-                                    echo (esc($aideTechnique['poidsMax'] / 1000) . " Kg");
+                                    echo (esc($aideTechnique['poidsMax'] / 1000) . "Kg");
                                 } ?>
                             </h6>
                         <?php
@@ -152,16 +152,16 @@
                             <h6 class="d-inline"> de
                                 <?php
                                 if ($aideTechnique['poidsMin'] < 999) {
-                                    echo (esc($aideTechnique['poidsMin']) . " g");
+                                    echo (esc($aideTechnique['poidsMin']) . "g");
                                 } else {
-                                    echo (esc($aideTechnique['poidsMin'] / 1000) . " Kg");
+                                    echo (esc($aideTechnique['poidsMin'] / 1000) . "Kg");
                                 } ?>
                                 à
                                 <?php
                                 if ($aideTechnique['poidsMax'] < 999) {
-                                    echo (esc($aideTechnique['poidsMax']) . " g");
+                                    echo (esc($aideTechnique['poidsMax']) . "g");
                                 } else {
-                                    echo (esc($aideTechnique['poidsMax'] / 1000) . " Kg");
+                                    echo (esc($aideTechnique['poidsMax'] / 1000) . "Kg");
                                 } ?>
                             </h6>
                         <?php
@@ -173,24 +173,141 @@
                     <div class="col-md-auto">
                         <h6 class="d-inline m-0 font-weight-bold text-primary">Est ajustable : </h6>
                         <h6 class="d-inline">
-                        <?php if(esc($aideTechnique['estAjustable'])== 0){
-                            echo("Non");
-                        }else{
-                            echo("Oui");
-                        }
-                        ?></h6>
+                            <?php if (esc($aideTechnique['estAjustable']) == 0) {
+                                echo ("Non.");
+                            } else if (esc($aideTechnique['estAjustable']) == 1) {
+                                echo ("Oui.");
+                            } else if (esc($aideTechnique['estAjustable']) == 2) {
+                                echo ("Sur certains modèles.");
+                            } else {
+                                echo ("Information inconnue.");
+                            }
+                            ?></h6>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-auto">
                         <h6 class="d-inline m-0 font-weight-bold text-primary">Dimensions plié : </h6>
-                        <h6 class="d-inline"><?= ucfirst(esc($aideTechnique['idDimPlie'])); ?></h6>
+                        <h6 class="d-inline">
+                            <?php
+                            if ($aideTechnique['idDimPlie'] === null) {
+                                echo ("Ne se plie pas.");
+                            }
+                            if (esc($aideTechnique['longueurMaxPlie']) != 0 && esc($aideTechnique['longueurMaxPlie']) != null) {
+                                if (esc($aideTechnique['longueurMinPlie']) === esc($aideTechnique['longueurMaxPlie'])) {
+                            ?>
+                                    <br> Longueur : environ <?= esc($aideTechnique['longueurMinPlie']); ?> cm
+                                <?php
+                                } else {
+                                ?>
+                                    <br> Longueur : de <?= esc($aideTechnique['longueurMinPlie']); ?> à <?= esc($aideTechnique['longueurMaxPlie']); ?> cm
+                                <?php
+                                }
+                            }
+                            if (esc($aideTechnique['largeurMaxPlie']) != 0 && esc($aideTechnique['largeurMaxPlie']) != null) {
+                                if (esc($aideTechnique['largeurMinPlie']) === esc($aideTechnique['largeurMaxPlie'])) {
+                                ?>
+                                    <br> Largeur : environ <?= esc($aideTechnique['largeurMaxPlie']); ?> cm
+                                <?php
+                                } else {
+                                ?>
+                                    <br> Largeur : de <?= esc($aideTechnique['largeurMinPlie']); ?> à <?= esc($aideTechnique['largeurMaxPlie']); ?> cm
+                                <?php
+                                }
+                            }
+                            if (esc($aideTechnique['hauteurMaxPlie']) != 0 && esc($aideTechnique['hauteurMaxPlie']) != null) {
+                                if (esc($aideTechnique['hauteurMinPlie']) === esc($aideTechnique['hauteurMaxPlie'])) {
+                                ?>
+                                    <br> Hauteur : environ <?= esc($aideTechnique['hauteurMinPlie']); ?> cm
+                                <?php
+                                } else {
+                                ?>
+                                    <br> Hauteur : de <?= esc($aideTechnique['hauteurMinPlie']); ?> à <?= esc($aideTechnique['hauteurMaxPlie']); ?> cm
+                            <?php
+                                }
+                            }
+                            ?>
+                        </h6>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-auto">
-                        <h6 class="d-inline m-0 font-weight-bold text-primary">Requiert 2 personnes : </h6>
-                        <h6 class="d-inline"><?= ucfirst(esc($aideTechnique['estMultiUtilisateur'])); ?></h6>
+                        <h6 class="d-inline m-0 font-weight-bold text-primary">Poids supporté : </h6>
+                        <?php
+                        if (esc($aideTechnique['idPSupporte']) === null) {
+                        ?>
+                            <h6 class="d-inline"> Information inconnue ou non applicable.</h6>
+                        <?php
+                        } else if (esc($aideTechnique['poidsMinSupporte']) >= esc($aideTechnique['poidsMaxSupporte'])) {
+                        ?>
+                            <h6 class="d-inline"> environ
+                                <?php
+                                if ($aideTechnique['poidsMaxSupporte'] < 999) {
+                                    echo (esc($aideTechnique['poidsMaxSupporte']) . "g");
+                                } else {
+                                    echo (esc($aideTechnique['poidsMaxSupporte'] / 1000) . "Kg");
+                                } ?>
+                            </h6>
+                        <?php
+                        } else {
+                        ?>
+                            <h6 class="d-inline"> de
+                                <?php
+                                if ($aideTechnique['poidsMinSupporte'] < 999) {
+                                    echo (esc($aideTechnique['poidsMinSupporte']) . "g");
+                                } else {
+                                    echo (esc($aideTechnique['poidsMinSupporte'] / 1000) . "Kg");
+                                } ?>
+                                à
+                                <?php
+                                if ($aideTechnique['poidsMaxSupporte'] < 999) {
+                                    echo (esc($aideTechnique['poidsMaxSupporte']) . "g");
+                                } else {
+                                    echo (esc($aideTechnique['poidsMaxSupporte'] / 1000) . "Kg");
+                                } ?>
+                            </h6>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-auto">
+                        <h6 class="d-inline m-0 font-weight-bold text-primary">Alimentation : </h6>
+                        <h6 class="d-inline">
+                            <?= esc($aideTechnique['estAlimenté']);?></h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-auto">
+                        <h6 class="d-inline m-0 font-weight-bold text-primary">Necessite une autre personne pour être utilisée : </h6>
+                        <h6 class="d-inline">
+                            <?php if (esc($aideTechnique['estMultiUtilisateur']) == 0) {
+                                echo ("Non.");
+                            } else if (esc($aideTechnique['estMultiUtilisateur']) == 1) {
+                                echo ("Oui.");
+                            } else if (esc($aideTechnique['estMultiUtilisateur']) == 2) {
+                                echo ("Sur certains modèles.");
+                            } else {
+                                echo ("Information inconnue.");
+                            }
+                            ?></h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-auto">
+                        <h6 class="d-inline m-0 font-weight-bold text-primary">S'utilise en exterieur : </h6>
+                        <h6 class="d-inline">
+                            <?php if (esc($aideTechnique['sutiliseEnExterieur']) == 0) {
+                                echo ("Non.");
+                            } else if (esc($aideTechnique['sutiliseEnExterieur']) == 1) {
+                                echo ("Oui.");
+                            } else if (esc($aideTechnique['sutiliseEnExterieur']) == 2) {
+                                echo ("Sur certains modèles.");
+                            } else {
+                                echo ("Information inconnue.");
+                            }
+                            ?></h6>
                     </div>
                 </div>
             </div>
