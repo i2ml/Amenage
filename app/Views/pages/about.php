5f96fb25f9f7b38c504c3645a1226958fd335762
Bast <bastien.last87@gmail.com>
2020-10-01 23:58:37 +0200

    Ajout de la page d'information

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,1 +1,26 @@
-about
+<div class="Box-body px-5 pb-5 justify-content-center text-center">
+    <article class="markdown-body entry-content container-lg" itemprop="text">
+        <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/d303327d7ea0f4c6062e2193c924549286ab5981/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3732373835343236383938373630353038332f3736303135303339333033353934383035322f4c4f474f53416e73666f6e64677269732e706e67"><img src="https://camo.githubusercontent.com/d303327d7ea0f4c6062e2193c924549286ab5981/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3732373835343236383938373630353038332f3736303135303339333033353934383035322f4c4f474f53416e73666f6e64677269732e706e67" alt="" data-canonical-src="https://cdn.discordapp.com/attachments/727854268987605083/760150393035948052/LOGOSAnsfondgris.png" style="max-width:100%;"></a></p>
+        <p>Aménâge est une application web permettant d’obtenir simplement et rapidement des informations sur les aides techniques en fonction de sa situation (état de santé, environnement existant et difficultés en découlant).</p>
+        <p>Open-source et gratuite, la solution est accessible à tous à l'adresse <a href="https://amenage.i2ml.fr" rel="nofollow">https://amenage.i2ml.fr</a></p>
+        <p>La plateforme est liée au logiciel d'amenagement Amenage3D, autre projet open-source de la fondation i2ml (<a href="https://github.com/i2ml/Amenage3D">https://github.com/i2ml/Amenage3D</a>), le site permet en effet d'obtenir des informations supplémentaires sur les aides techniques présente dans le logiciel.</p>
+        <p><a href="https://github.com/i2ml/amenage"><img src="https://camo.githubusercontent.com/b2c7d42081c9ed99eed66abdf3219a4987336bda/68747470733a2f2f696d672e736869656c64732e696f2f6769746875622f73746172732f69326d6c2f616d656e6167652e7376673f6c6162656c3d5374617273267374796c653d736f6369616c" alt="" data-canonical-src="https://img.shields.io/github/stars/i2ml/amenage.svg?label=Stars&amp;style=social" style="max-width:100%;"></a></p>
+        <h1 class="m-4">Fonctionnalités principales</h1>
+        <h2 class="m-4">Cibler votre besoin</h2>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/c4f5ed65121edf8f7f7fcb3dcc01fa06d72b78b0/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334313039303534343438343335322f756e6b6e6f776e2e706e67"><img src="https://camo.githubusercontent.com/c4f5ed65121edf8f7f7fcb3dcc01fa06d72b78b0/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334313039303534343438343335322f756e6b6e6f776e2e706e67" alt="" data-canonical-src="https://cdn.discordapp.com/attachments/694913844174389329/761341090544484352/unknown.png" style="max-width:100%;"></a></p>
+        <p>Determinez simplement en quatre questions maximum quelle aide technique correspond à votre besoin.</p>
+        <h2 class="m-4">Recherche approfondie</h2>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/3e5287c9a3e93a9993716058f62351ea06fc4682/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334313936363437343534333133342f756e6b6e6f776e2e706e67"><img src="https://camo.githubusercontent.com/3e5287c9a3e93a9993716058f62351ea06fc4682/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334313936363437343534333133342f756e6b6e6f776e2e706e67" alt="" data-canonical-src="https://cdn.discordapp.com/attachments/694913844174389329/761341966474543134/unknown.png" style="max-width:100%;"></a></p>
+        <p>Filtrez avec précision les résultats pour trouver exactement ce dont vous avez besoin</p>
+        <h1 class="m-4">Les acteurs du projet</h1>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/02c7ebd98718cd3cba43e41b6ead98189f3e5f9c/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334323234343934363138323135342f756e6b6e6f776e2e706e67"><img src="https://camo.githubusercontent.com/02c7ebd98718cd3cba43e41b6ead98189f3e5f9c/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334323234343934363138323135342f756e6b6e6f776e2e706e67" alt="" data-canonical-src="https://media.discordapp.net/attachments/694913844174389329/761342244946182154/unknown.png" style="max-width:100%;"></a></p>
+        <h1 class="m-4">Guide d'installation</h1>
+        <p>Vous souhaitez contribuer au developpement de la plateforme ? Suivez ce guide pour lancer l'application web.</p>
+        <ul>
+            <li>Clonez le projet <code>git clone https://github.com/i2ml/Amenage.git</code></li>
+            <li>Suivez le guide d'installation mannuel de code igniter <a href="https://codeigniter.com/user_guide/installation/installing_manual.html" rel="nofollow">(lien direct)</a>.</li>
+            <li>Décompressez le fichier public/assets package.zip</li>
+            <li>Renommez le fichier <code>env</code> en <code>.env</code> et complétez les champs qui ne sont pas commentés à l'aide de la commande <code>nano .env</code></li>
+        </ul>
+    </article>
+</div>
