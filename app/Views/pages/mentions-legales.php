d0f800b741a0099b826faf2aaccca2c668a05541
Claire <claire.rolley@i2ml.fr>
2020-10-02 17:03:32 +0200

    MAJ page "about" et mentions légales

--- app/Views/pages/mentions-legales.php
+++ app/Views/pages/mentions-legales.php
@@ -1,1 +1,55 @@
-Hello world
+<div class="Box-body px-5 pb-5 justify-content-center text-center">
+    <div class="container-lg">
+        <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/d303327d7ea0f4c6062e2193c924549286ab5981/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3732373835343236383938373630353038332f3736303135303339333033353934383035322f4c4f474f53416e73666f6e64677269732e706e67"><img src="https://camo.githubusercontent.com/d303327d7ea0f4c6062e2193c924549286ab5981/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3732373835343236383938373630353038332f3736303135303339333033353934383035322f4c4f474f53416e73666f6e64677269732e706e67" alt="" data-canonical-src="https://cdn.discordapp.com/attachments/727854268987605083/760150393035948052/LOGOSAnsfondgris.png" style="max-width:100%;"></a></p>
+        
+        <h1 class="m-4">Mentions légales</h1>
+        
+        <p> </p>
+        
+        <h2 class="m-4">Responsabilités</h2>
+
+<p>Fondation i2ml
+L’Altis, 165 rue Philippe Maupas
+30000 Nîmes – France
++33 4 11 25 01 01
+Siret : 529 686 776 00012</p>
+
+<p>Responsable de la publication : Benoit Roig, président de la Fondation i2ml</p>
+
+<p>Hébergeur web : OVH
+2 rue Kellermann
+59100 Roubaix – France</p>
+
+<h2 class="m-4">Conditions d’utilisation</h2>
+
+<p>Les utilisateurs du site sont tenus de respecter les dispositions de la loi relative à l’informatique, aux fichiers et aux libertés, dont la violation est passible de sanctions pénales. Ils doivent notamment s’abstenir, s’agissant d’informations nominatives auxquelles ils accèdent, de toute collecte, de toute utilisation détournée et d’une manière générale, de tout acte susceptible de porter atteinte à la vie privée ou à la réputation des personnes.</p>
+
+
+<h2 class="m-4">Limite de responsabilité</h2>
+
+<p>La Fondation i2ml décline toute responsabilité pour toute imprécision, inexactitude ou omission portant sur des informations disponibles sur le site ; pour tout dommage résultant d’une intrusion frauduleuse d’un tiers ayant entraîné une modification des informations mises à la disposition sur le site ; et plus généralement pour tous dommages, directs ou indirects, qu’elles qu’en soient les causes, origines, nature ou conséquences, provoqués à raison de l’accès de quiconque au site ou de l’impossibilité d’y accéder, de même que l’utilisation du site et/ou du crédit accordé à une quelconque information provenant directement ou indirectement de ce dernier.</p>
+<h2 class="m-4">Traitement des données à caractère personnel</h2>
+
+<p>Conformément à la loi n°78-17 du 6 janvier 1978 modifiée en 2004 relative à l’informatique, aux fichiers et aux libertés, toute personne peut obtenir communication et, le cas échéant, rectification, suppression ou opposition des informations la concernant. Ce droit peut être exercé par courriel à l’adresse : contact@i2ml.fr</p>
+
+<p>Le site s’engage à garantir la confidentialité des informations recueillies auprès des personnes par le biais du formulaire de contact. Ces informations ne sont en aucun cas cédées ou rendues accessibles à une entité tierce.</p>
+
+<p>Vous pouvez trouver la loi relative à l’informatique, aux fichiers et aux libertés sur le site de la Commission Nationale de l’Informatique et des Libertés, à l’adresse suivante : www.cnil.fr.</p>
+<h2 class="m-4">Cookies</h2>
+
+<p>En vue d’adapter le site aux demandes de ses visiteurs, la Fondation i2ml mesure le nombre de visites, le nombre de pages vues ainsi que l’activité des visiteurs sur le site et leur fréquence de retour. Nous n’avons pas d’autres exploitations des données.</p>
+
+<p>Le site utilise Google Analytics, un service d’analyse de trafic des sites internet. Google Analytics utilise des cookies qui sont des fichiers texte placés sur votre ordinateur, pour aider le site internet à analyser l’utilisation du site par ses utilisateurs. Les données générées par les cookies concernent votre utilisation du site (y compris votre adresse IP).</p>
+
+<p>Vous pouvez désactiver l’utilisation de cookies en sélectionnant les paramètres appropriés de votre navigateur. Cependant, une telle désactivation pourrait empêcher l’utilisation de certaines fonctionnalités de ce site.</p>
+<h2 class="m-4">Loi applicable</h2>
+
+<p>Les sites de la Fondation i2ml et leurs contenus sont créés conformément aux règles applicables en France, même s’ils sont accessibles à d’autres utilisateurs francophones.</p>
+
+<p>Par ailleurs, dans le cas d’une divergence entre les informations présentées dans la version française et la version anglaise du site internet de la Fondation i2ml, les informations présentées dans la version française prévalent.</p>
+
+        
+       
+    </div>
+
+</div>
