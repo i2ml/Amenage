23b1e5af6c69419f6b3cef36d7666e03b021159c
BastLast <bastien.last87@gmail.com>
2020-11-19 11:34:26 +0100

    Ajout message at introuvable

--- app/Views/aideTechnique/overview.php
+++ app/Views/aideTechnique/overview.php
@@ -92,8 +92,13 @@
                         </div>
                     </div>
                     <div class="row justify-content-md-center">
-                        <button class="btn btn-primary mr-3" id="reset-filter" type="button">Réinitialiser les filtres</button>
-                        <button class="btn btn-primary ml-3" id="submit-filter" type="button">Appliquer les filtres</button>
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center mt-3">
+                            <button class="btn btn-primary" id="reset-filter" type="button">Réinitialiser les filtres</button>
+                        </div>
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center mt-3">
+                            <button class="btn btn-primary" id="submit-filter" type="button">Appliquer les filtres</button>
+
+                        </div>
                     </div>
                 </form>
             </div>
@@ -124,13 +129,20 @@
     </div>
 
 <?php else :
-    // TODO : faire la partie ci-dessous
 ?>
-
-    <h3>Pas de résultat</h3>
-
-    <p>Aucune aide technique n'a été trouvée. Tentez d'élargir vos critères de sélection</p>
-
+    <div class="container mx-3">
+        <div class="row d-flex justify-content-center">
+            <h3>Nous n'avons pas trouvé de résultat :/</h3>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <p>Aucune aide technique n'a été trouvée. Tentez d'élargir vos critères de sélection !</p>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-10 col-sm-8 col-lg-6 col-xl-4 align-self-center">
+                <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="<?php echo base_url('assets/img/Character.jpg'); ?>" alt="No result illustration">
+            </div>
+        </div>
+    </div>
 <?php endif ?>
 
 
--- app/Views/categorie/overview.php
+++ app/Views/categorie/overview.php
@@ -26,11 +26,19 @@
         <?php endforeach; ?>
 </div>
 <?php else :
-        // TODO : faire la partie ci-dessous
 ?>
-
-    <h3>Pas de résultat</h3>
-
-    <p>Aucune catégorie n'a été trouvée. Tentez d'élargir vos critères de sélection</p>
+    <div class="container mx-3">
+        <div class="row d-flex justify-content-center">
+            <h3>Nous n'avons pas trouvé de résultat :/</h3>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <p>Aucune aide technique n'a été trouvée. Tentez d'élargir vos critères de sélection !</p>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-10 col-sm-8 col-lg-6 col-xl-4 align-self-center">
+                <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="<?php echo base_url('assets/img/Character.jpg'); ?>" alt="No result illustration">
+            </div>
+        </div>
+    </div>
 
 <?php endif ?>
