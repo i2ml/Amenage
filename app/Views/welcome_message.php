3443b4802ec0564b87603fa4f50372e7135c8970
BastLast <bastien.last87@gmail.com>
2020-12-03 00:40:42 +0100

    fixes home page header

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -28,13 +28,13 @@
 		<!-- bg-gradient-primary -->
 		<div class="hero-infos container">
 
-			<div class="row align-items-end hero-titlerow d-flex justify-content-center">
+			<div class="row align-items-end hero-titlerow d-flex justify-content-center mt-sm-0 mt-sm-5 ">
 				<h1>
 					<div class="d-sm-block d-none title">Aménâge - Faciliter les amenagements pour l'autonomie</div>
 					<div class="d-sm-none smalltitle">Aménâge - Faciliter les amenagements pour l'autonomie</div>
 				</h1>
 			</div>
-			<div class="row align-items-end hero-subtitlerow justify-content-center mt-3">
+			<div class="row d-md-flex d-none align-items-end hero-subtitlerow justify-content-center mt-3">
 				<h2 class="col-6">
 					<div class="d-sm-block d-none smalltitle">Un catalogue d'aides techniques pour découvrir et s'informer</div>
 				</h2>
