9c29618c5052298ffa042c6976df6e458b0a000a
BastLast <bastien.last87@gmail.com>
2020-12-03 00:46:01 +0100

    updated credits

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,27 +1,27 @@
 <div class="Box-body px-5 pb-5 justify-content-center text-center">
     <div class="container-lg">
         <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/d303327d7ea0f4c6062e2193c924549286ab5981/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3732373835343236383938373630353038332f3736303135303339333033353934383035322f4c4f474f53416e73666f6e64677269732e706e67"><img src="https://camo.githubusercontent.com/d303327d7ea0f4c6062e2193c924549286ab5981/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3732373835343236383938373630353038332f3736303135303339333033353934383035322f4c4f474f53416e73666f6e64677269732e706e67" alt="" data-canonical-src="https://cdn.discordapp.com/attachments/727854268987605083/760150393035948052/LOGOSAnsfondgris.png" style="max-width:100%;"></a></p>
-        
+
         <h1 class="m-4">Qui sommes-nous ?</h1>
-        
+
         <p>Aménâge est une application web permettant d’obtenir simplement et rapidement des informations sur les aides techniques en fonction de sa situation (état de santé, environnement existant et difficultés en découlant).</p>
         <p>Open-source et gratuite, la solution est accessible à tous à l'adresse <a href="https://amenage.i2ml.fr" rel="nofollow">https://amenage.i2ml.fr</a></p>
         <p>La plateforme est liée au logiciel d'amenagement Amenage3D, autre projet open-source de la fondation i2ml (<a href="https://github.com/i2ml/Amenage3D">https://github.com/i2ml/Amenage3D</a>), le site permet en effet d'obtenir des informations supplémentaires sur les aides techniques présente dans le logiciel.</p>
         <p>Aménâge est développé par la Fondation i2ml et financé par la CFPPA du Gard et le Conseil Départemental du Gard. </p>
-        
-        
+
+
         <p><a href="https://github.com/i2ml/amenage"><img src="https://camo.githubusercontent.com/b2c7d42081c9ed99eed66abdf3219a4987336bda/68747470733a2f2f696d672e736869656c64732e696f2f6769746875622f73746172732f69326d6c2f616d656e6167652e7376673f6c6162656c3d5374617273267374796c653d736f6369616c" alt="" data-canonical-src="https://img.shields.io/github/stars/i2ml/amenage.svg?label=Stars&amp;style=social" style="max-width:100%;"></a></p>
-        
+
         <h1 class="m-4">Aménâge, c'est quoi ?</h1>
         <h2 class="m-4">Le constat de base</h2>
         <p>En 2030, un tiers des Français aura plus de 60 ans. Dans ce contexte, favoriser l’autonomie à domicile des seniors le plus longtemps possible devient une préoccupation sociétale et économique majeure. Pour prévenir la perte d’autonomie et encourager le maintien à domicile, les personnes âgées et/ou en situation de handicap peuvent avoir recours à des aides techniques. </p>
         <p>Les aides techniques permettent à la personne de pallier une perte d’autonomie dans diverses situations du quotidien (toilette, mobilité, etc.). Or, les aides techniques ne sont pas suffisamment mobilisées ou pas toujours à bon escient. Ainsi, il apparaît que :
-        <ul style="text-align:justify;">
-            <li>les personnes âgées et/ou handicapées n’ont pas forcément connaissance des aides techniques et peuvent avoir du mal à visualiser leur intérêt et/ou leur intégration dans leur domicile.</li>
-            <li>une démonstration de l’usage de certaines aides techniques est complexe du fait de leur nécessaire installation (on ne peut pas juste montrer un interrupteur automatique, il faut pouvoir l’intégrer au domicile pour démontrer son intérêt)</li>
-            <li>les travailleurs sociaux et services à la personne, de par leur formation, ne disposent pas de des compétences et connaissances nécessaires à la prescription et préconisation d’aides techniques. Ainsi, ils se trouvent bien souvent obligés faire preuve d’ingéniosité pour convaincre les individus des bénéfices que pourraient apporter les aides techniques. Si des photos et des explications sont nécessaires à cette compréhension, elles ne sont pas toujours suffisantes pour permettre à l’utilisateur de projeter de manière concrète l’aide dans son environnement quotidien.</li>
-            <li>les aides techniques sont mal acceptées parce qu’elles stigmatisent l’usager.</li>
-        </ul>
+            <ul style="text-align:justify;">
+                <li>les personnes âgées et/ou handicapées n’ont pas forcément connaissance des aides techniques et peuvent avoir du mal à visualiser leur intérêt et/ou leur intégration dans leur domicile.</li>
+                <li>une démonstration de l’usage de certaines aides techniques est complexe du fait de leur nécessaire installation (on ne peut pas juste montrer un interrupteur automatique, il faut pouvoir l’intégrer au domicile pour démontrer son intérêt)</li>
+                <li>les travailleurs sociaux et services à la personne, de par leur formation, ne disposent pas de des compétences et connaissances nécessaires à la prescription et préconisation d’aides techniques. Ainsi, ils se trouvent bien souvent obligés faire preuve d’ingéniosité pour convaincre les individus des bénéfices que pourraient apporter les aides techniques. Si des photos et des explications sont nécessaires à cette compréhension, elles ne sont pas toujours suffisantes pour permettre à l’utilisateur de projeter de manière concrète l’aide dans son environnement quotidien.</li>
+                <li>les aides techniques sont mal acceptées parce qu’elles stigmatisent l’usager.</li>
+            </ul>
         </p>
         <p>Par ailleurs, il est bien souvent nécessaire que la personne prenne un temps de réflexion et d’acceptation de sa perte d’autonomie avant de se procurer une aide technique. En ce sens, il n’existe pas d’outil (au regard de l’état de l’art actuel) pour accompagner la personne dans ce processus d’acceptation et d’intégration des aides techniques au domicile. </p>
         <p>Aménâge a donc été développé pour fournir un outil numérique de sensibilisation, disponible pour le grand public. Aménâge est également pertinent pour les professionnels de santé qui ont pour mission d’accompagner, informer, conseiller les personnes fragiles, sans pour autant avoir toutes les informations à portée de main. </p>
@@ -38,7 +38,7 @@
         <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/3e5287c9a3e93a9993716058f62351ea06fc4682/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334313936363437343534333133342f756e6b6e6f776e2e706e67"><img src="https://camo.githubusercontent.com/3e5287c9a3e93a9993716058f62351ea06fc4682/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334313936363437343534333133342f756e6b6e6f776e2e706e67" alt="" data-canonical-src="https://cdn.discordapp.com/attachments/694913844174389329/761341966474543134/unknown.png" style="max-width:100%;"></a></p>
         <p>Composé des aides techniques les plus répandues, le catalogue d'Aménâge va vous permettre de naviguer entre différentes catégories d'aides techniques pour en sélectionner quelques unes. Pour chaque aide technique, Aménâge vous diffuse un certain nombre d'informations telles qu'une brève description, le prix, le montant de remboursement, les dimensions... Filtrez avec précision les résultats pour trouver exactement ce dont vous avez besoin. </p>
         <p>Ces informations sont celles de la CPAM et celles généralement observées chez les distributeurs d'aides techniques : elles résultent d'une veille globale réalisée par l'équipe d'Aménâge pour vous transmettre les informations qui, en général, sont diffusées par ces acteurs. C'est par exemple pourquoi vous trouverez des fourchettes de prix et non pas un prix précis. De la même manière, vous ne trouverez pas de modèle spécifique à commander : Aménâge n'a pas vocation à vous orienter vers un produit, mais plutôt vers une catégorie de solutions pour vous permettre de visualiser le type de solution que pourront vous préconiser les professionnels compétents.</p>
-        
+
 
 
 
@@ -48,19 +48,19 @@
         <h2 class="m-4">Ok, j'ai mes informations. Et maintenant ?</h2>
         <p>Une fois que vous avez trouvé les informations que vous cherchiez sur Aménâge, vous devriez être en mesure d'avoir une idée plus claire de vos besoins et des solutions potentielles associées. Il ne vous reste plus qu'à prendre contact avec des professionnels compétents qui sauront évaluer précisément vos besoins et qui sauront vous guider dans le choix des solutions pertinentes adaptées à votre situation propre. </p>
         <p>Pour cela, l'équipe d'Aménâge conseille de vous rapprocher de :
-        <ul style="text-align:justify;">
-        <li> du CCAS ou de la mairie de votre commune de résidence : ils sauront vous guider dans les démarches à accomplir. </li>
-        <li> si vous êtes bénéficiaire de l'APA (Allocation Personnalisée d'Autonomie), vous pouvez vous adresser à la personne en charge de votre dossier. Cette dernière saura certainement en mesure de vous proposer un accompagnement.</li>
-        <li> vous pouvez également vous tourner vers un ergothérapeute, libéral ou non, pour que celui-ci évalue vos besoins et vous préconise des solutions adaptées.</li>
-        </ul>
+            <ul style="text-align:justify;">
+                <li> du CCAS ou de la mairie de votre commune de résidence : ils sauront vous guider dans les démarches à accomplir. </li>
+                <li> si vous êtes bénéficiaire de l'APA (Allocation Personnalisée d'Autonomie), vous pouvez vous adresser à la personne en charge de votre dossier. Cette dernière saura certainement en mesure de vous proposer un accompagnement.</li>
+                <li> vous pouvez également vous tourner vers un ergothérapeute, libéral ou non, pour que celui-ci évalue vos besoins et vous préconise des solutions adaptées.</li>
+            </ul>
         </p>
 
-     
-        
+
+
         <h1 class="m-4">Les acteurs du projet</h1>
         <p><a target="_blank" rel="noopener noreferrer" href="https://camo.githubusercontent.com/02c7ebd98718cd3cba43e41b6ead98189f3e5f9c/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334323234343934363138323135342f756e6b6e6f776e2e706e67"><img src="https://camo.githubusercontent.com/02c7ebd98718cd3cba43e41b6ead98189f3e5f9c/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3639343931333834343137343338393332392f3736313334323234343934363138323135342f756e6b6e6f776e2e706e67" alt="" data-canonical-src="https://media.discordapp.net/attachments/694913844174389329/761342244946182154/unknown.png" style="max-width:100%;"></a></p>
-        
-        
+
+
         <h1 class="m-4">Guide d'installation</h1>
         <p>Vous souhaitez contribuer au developpement de la plateforme ? Suivez ce guide pour lancer l'application web.</p>
         <ul style="text-align:justify;">
@@ -72,6 +72,7 @@
 
         <h1 class="m-4">Crédits</h1>
         <a href='https://www.freepik.com/vectors/banner'>Banner vector created by upklyak - www.freepik.com</a>
+        <a href='https://www.freepik.com/vectors/people'>others vectors created by Macrovector - www.freepik.com</a>
     </div>
 
 </div>
