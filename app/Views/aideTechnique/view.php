bee32698554ded72f480eb0cf8666c7d4a233d5c
BastLast <bastien.last87@gmail.com>
2020-12-08 23:37:59 +0100

    added fil d'arianne on aide technique

--- app/Views/aideTechnique/view.php
+++ app/Views/aideTechnique/view.php
@@ -1,4 +1,17 @@
 <div class="row mt-md-3 ml-md-3">
+    <h5 class="h5 mb-0 text-gray-800 ml-5">
+        <a href="<?php echo base_url('aideTechniques/categorie') ?>">
+            <h5 class="d-inline">Liste des catégories </h5>
+        </a>
+        >
+        <a href="<?php echo base_url('aideTechniques/categorie/' . esc($aideTechnique['idCat'])) ?>">
+            <h5 class="d-inline"><?= ucfirst(esc($aideTechnique['nomCat'])); ?> </h5>
+        </a>
+        >
+        <?= ucfirst(esc($aideTechnique['nom'])); ?>
+    </h5>
+</div>
+<div class="row mt-md-3 ml-md-3">
     <h1 class="h3 mb-0 text-gray-800 ml-5"><?= ucfirst(esc($aideTechnique['nom'])); ?></h1>
 </div>
 <div class="row">
