e171a8ae9866d2c00085ad26886844001b517e11
BastLast <bastien.last87@gmail.com>
2020-12-12 17:18:04 +0100

    improved wave

--- app/Views/accueil.php
+++ app/Views/accueil.php
@@ -20,7 +20,7 @@
 </head>
 
 <body>
-	<div class="hero-bg">
+	<div class="container-fluid p-0 mb-4 hero-bg">
 
 		<header>
 			<a class="logo" href="<?php echo base_url() ?>"><img class="ml-5 mt-2 p-2" src="<?php echo base_url('assets/img/LOGOSAnsfondblanc.png'); ?>" alt="logo"></a>
@@ -49,49 +49,49 @@
 				</div>
 			</div>
 		</div>
-		<div class="waves">
-			<svg width="100%" height="175px" fill="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+		<div class="row mt-3">
+			<svg viewBox="0 0 1440 150" preserveAspectRatio="none" fill="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
 				<linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
 					<stop offset="0%" stop-color="#fff" />
 				</linearGradient>
 				<path fill="url(#grad1)" d="
                     M0 67
                     C 273,183
-                    822,-40
-                    1920.00,106
+                    822,0
+                    1440.00,106
                     V 359
                     H 0
                     V 67
                     Z">
-					<animate repeatCount="indefinite" fill="url(#grad1)" attributeName="d" dur="35s" attributeType="XML" values="
-                    M0 77
+					<animate repeatCount="indefinite" fill="url(#grad1)" attributeName="d" dur="5s" attributeType="XML" values="
+                    M0 67
                     C 473,283
-                    822,-40
-                    1920,116
+                    822,0
+                    1440,116
                     V 359
                     H 0
                     V 67
                     Z;
-                    M0 77
-                    C 473,-40
+                    M0 67
+                    C 473,0
                     1222,283
-                    1920,136
+                    1440,67
                     V 359
                     H 0
                     V 67
                     Z;
-                    M0 77
+                    M0 67
                     C 973,260
-                    1722,-53
-                    1920,120
+                    1722,0
+                    1440,120
                     V 359
                     H 0
                     V 67
                     Z;
-                    M0 77
+                    M0 67
                     C 473,283
-                    822,-40
-                    1920,116
+                    822,0
+                    1440,116
                     V 359
                     H 0
                     V 67
@@ -117,11 +117,8 @@ if we ever need a subtitle here
 					</div>
 				</div> 
 			-->
-				<div class="row align-items-center mb-5">
-
-					<div class="col-xl-1 d-xl-block d-none">
-					</div>
-					<div class="col-lg-5 col-xl-4">
+				<div class="row justify-content-center">
+					<div class="col-lg-12 mt-lg-5 col-xl-6">
 						<form action="<?php echo base_url('aideTechniques') ?>" method="post">
 							<div class="input-group">
 								<input type="text" name="searchBar" class="form-control bg-darkerlight border-0 small" placeholder="Rechercher une aide technique..." aria-label="Search" aria-describedby="basic-addon2" required>
@@ -133,17 +130,21 @@ if we ever need a subtitle here
 							</div>
 						</form>
 					</div>
-					<div class="col-xl-1 d-xl-block d-none">
-					</div>
-					<div class="col-lg-7 col-xl-5">
-						<div class="row d-flex justify-content-center m-3 my-5">
-							<a href="categories" class="button btn-dark buttonLarge w-100 p-3 px-5 d-flex justify-content-center">
+				</div>
+				<div class="row justify-content-center mb-5">
+					<div class="d-flex justify-content-center m-3 my-5">
+						<div class="col-7">
+							<a href="categories" class=" m-5 button btn-dark buttonLarge mt-3 w-100 p-3 px-5 d-flex justify-content-center">
 								<p class="m-auto">
 									<i class="fa fa-folder-open buttonIcon"></i>
 									Parcourir une catégorie
 								</p>
 							</a>
-							<a href="aideTechniques" class="button btn-dark buttonLarge mt-3 w-100 p-3 px-5 d-flex justify-content-center">
+						</div>
+						<div class="col-1">
+						</div>
+						<div class="col-7">
+							<a href="aideTechniques" class="m-5 button btn-dark buttonLarge mt-3 w-100 p-3 px-5 d-flex justify-content-center">
 								<p class="m-auto">
 									<i class="fas fa-list-ul buttonIcon"></i>
 									Voir toutes les aides techniques
@@ -151,72 +152,72 @@ if we ever need a subtitle here
 							</a>
 						</div>
 					</div>
-					<div class="col-xl-1 d-xl-block d-none">
-					</div>
 				</div>
-				<div class="row justify-content-center my-5">
-					<div class="d-block d-md-none col-8 align-self-center text-center">
-						<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="<?php echo base_url('assets/img/dowloadamenage3d.jpg'); ?>" alt="No result illustration">
-					</div>
-					<div class="col-md-5 col-12 align-self-center text-center justify-content-center">
-						<h2 class="h2 mb-3 subtitle bold">Installez Amenage 3D !</h2>
-						<h3 class="h3 mb-0">Visualisez les aides techniques en 3D dans votre domicile !</h3>
-						<div class="row justify-content-center my-5">
-							<div class="col-md-12 col-lg-10 col-xl-8 align-self-center text-center justify-content-center">
-								<a href="<?php echo base_url('3d') ?>" class="button btn-dark buttonLarge w-100 p-3 px-5 d-flex justify-content-center">
-									<p class="m-auto">
-										<i class="fa fa-download buttonIcon"></i>
-										Télécharger gratuitement
-									</p>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class="d-none d-md-block col-5 align-self-center text-center">
-						<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="<?php echo base_url('assets/img/dowloadamenage3d.jpg'); ?>" alt="No result illustration">
-					</div>
-				</div>
-
-				<div class="row justify-content-center">
-					<h2 class="h3 mb-0 mt-5">Les acteurs du projet</h2>
-				</div>
-				<div class="row justify-content-center">
-					<div class="card shadow mb-4 m-3">
-						<a class="stretched-link" href="https://i2ml.fr">
-						</a>
-						<div class="card-body">
-							<div class="text-center">
-								<h6 class="m-0 font-weight-bold">Fondation i2ml</h6>
-								<img class="img-fluid px-3 px-sm-4 mt-3 mb-4 img-verysmall" src="<?php echo base_url('assets/img/Logo_Square.png'); ?>" alt="logo partenaire">
-							</div>
-						</div>
-					</div>
-					<div class="card shadow mb-4 m-3">
-						<a class="stretched-link" href="https://www.gard.fr/toute-lactu-du-departement-du-gard/autonomie-la-conference-des-financeurs-lance-son-appel-a-initiatives-2020.html" <div class="text-center">
-						</a>
-						<div class="card-body">
-							<div class="text-center">
-								<h6 class="m-0 font-weight-bold">CFPPA 30</h6>
-								<img class="img-fluid px-3 px-sm-4 mt-3 mb-4 img-verysmall" src="<?php echo base_url('assets/img/Logo CFPPA30 Gard.png'); ?>" alt="logo partenaire">
-							</div>
-						</div>
-					</div>
-					<div class="card shadow mb-4 m-3">
-						<a class="stretched-link" href="https://www.gard.fr/accueil.html" <div class="text-center">
-						</a>
-						<div class="card-body">
-							<h6 class="m-0 font-weight-bold">Département du gard</h6>
-							<img class="img-fluid px-3 px-sm-4 mt-1 mb-1 img-verysmall" src="<?php echo base_url('assets/img/logo_gard_2019_rvb_1000X1000.jpg'); ?>" alt="logo partenaire">
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row justify-content-center pb-5">
-				<p>
-					<a href="mentions-legales">Mentions légales</a>
-				</p>
 			</div>
 		</div>
+		<div class="row justify-content-center my-5">
+			<div class="d-block d-md-none col-8 align-self-center text-center">
+				<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="<?php echo base_url('assets/img/dowloadamenage3d.jpg'); ?>" alt="No result illustration">
+			</div>
+			<div class="col-md-5 col-12 align-self-center text-center justify-content-center">
+				<h2 class="h2 mb-3 subtitle bold">Installez Amenage 3D !</h2>
+				<h3 class="h3 mb-0">Visualisez les aides techniques en 3D dans votre domicile !</h3>
+				<div class="row justify-content-center my-5">
+					<div class="col-md-12 col-lg-10 col-xl-8 align-self-center text-center justify-content-center">
+						<a href="<?php echo base_url('3d') ?>" class="button btn-dark buttonLarge w-100 p-3 px-5 d-flex justify-content-center">
+							<p class="m-auto">
+								<i class="fa fa-download buttonIcon"></i>
+								Télécharger gratuitement
+							</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="d-none d-md-block col-5 align-self-center text-center">
+				<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="<?php echo base_url('assets/img/dowloadamenage3d.jpg'); ?>" alt="No result illustration">
+			</div>
+		</div>
+
+		<div class="row justify-content-center">
+			<h2 class="h3 mb-0 mt-5">Les acteurs du projet</h2>
+		</div>
+		<div class="row justify-content-center">
+			<div class="card shadow mb-4 m-3">
+				<a class="stretched-link" href="https://i2ml.fr">
+				</a>
+				<div class="card-body">
+					<div class="text-center">
+						<h6 class="m-0 font-weight-bold">Fondation i2ml</h6>
+						<img class="img-fluid px-3 px-sm-4 mt-3 mb-4 img-verysmall" src="<?php echo base_url('assets/img/Logo_Square.png'); ?>" alt="logo partenaire">
+					</div>
+				</div>
+			</div>
+			<div class="card shadow mb-4 m-3">
+				<a class="stretched-link" href="https://www.gard.fr/toute-lactu-du-departement-du-gard/autonomie-la-conference-des-financeurs-lance-son-appel-a-initiatives-2020.html" <div class="text-center">
+				</a>
+				<div class="card-body">
+					<div class="text-center">
+						<h6 class="m-0 font-weight-bold">CFPPA 30</h6>
+						<img class="img-fluid px-3 px-sm-4 mt-3 mb-4 img-verysmall" src="<?php echo base_url('assets/img/Logo CFPPA30 Gard.png'); ?>" alt="logo partenaire">
+					</div>
+				</div>
+			</div>
+			<div class="card shadow mb-4 m-3">
+				<a class="stretched-link" href="https://www.gard.fr/accueil.html" <div class="text-center">
+				</a>
+				<div class="card-body">
+					<h6 class="m-0 font-weight-bold">Département du gard</h6>
+					<img class="img-fluid px-3 px-sm-4 mt-1 mb-1 img-verysmall" src="<?php echo base_url('assets/img/logo_gard_2019_rvb_1000X1000.jpg'); ?>" alt="logo partenaire">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row justify-content-center pb-5">
+		<p>
+			<a href="mentions-legales">Mentions légales</a>
+		</p>
+	</div>
+	</div>
 	</div>
 	</div>
 
--- app/Views/amenage3d.php
+++ app/Views/amenage3d.php
@@ -49,49 +49,49 @@
 				</div>
 			</div>
 		</div>
-		<div class="waves">
-			<svg width="100%" height="175px" fill="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+		<div class="row mt-3">
+			<svg viewBox="0 0 1440 150" preserveAspectRatio="none" fill="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
 				<linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
 					<stop offset="0%" stop-color="#fff" />
 				</linearGradient>
 				<path fill="url(#grad1)" d="
                     M0 67
                     C 273,183
-                    822,-40
-                    1920.00,106
+                    822,0
+                    1440.00,106
                     V 359
                     H 0
                     V 67
                     Z">
-					<animate repeatCount="indefinite" fill="url(#grad1)" attributeName="d" dur="35s" attributeType="XML" values="
-                    M0 77
+					<animate repeatCount="indefinite" fill="url(#grad1)" attributeName="d" dur="5s" attributeType="XML" values="
+                    M0 67
                     C 473,283
-                    822,-40
-                    1920,116
+                    822,0
+                    1440,116
                     V 359
                     H 0
                     V 67
                     Z;
-                    M0 77
-                    C 473,-40
+                    M0 67
+                    C 473,0
                     1222,283
-                    1920,136
+                    1440,67
                     V 359
                     H 0
                     V 67
                     Z;
-                    M0 77
+                    M0 67
                     C 973,260
-                    1722,-53
-                    1920,120
+                    1722,0
+                    1440,120
                     V 359
                     H 0
                     V 67
                     Z;
-                    M0 77
+                    M0 67
                     C 473,283
-                    822,-40
-                    1920,116
+                    822,0
+                    1440,116
                     V 359
                     H 0
                     V 67
