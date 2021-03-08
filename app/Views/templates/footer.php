17c2399f78107b9bf45bd91be9415447484584b4
BastLast <bastien.last87@gmail.com>
2021-03-08 10:16:44 +0100

    removed unused import

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -9,8 +9,6 @@
 </footer>
 
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url('assets/js/generalScript.js') ?>" type="text/javascript"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
